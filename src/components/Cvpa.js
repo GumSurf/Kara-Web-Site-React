2ec1c45d22fc5786daec97bb03a998e69904ab97
GumSurf <gabriel.christe@gmail.com>
2023-06-21 23:45:37 +0200

    Suppr : pas utile donc ajouter dans d'autres components car trop petit

--- src/components/Cvpa.js
+++
@@ -1,9 +0,0 @@
-function Cvpa() {
-    return (
-        <div className="background-sea">
-            <h1>Chez vous, partout et ailleurs</h1>
-        </div>
-    )
-}
-
-export default Cvpa
--- src/components/Logo.js
+++
@@ -1,9 +0,0 @@
-import ImgLogo from '../assets/LOGO.png'
-
-function Logo() {
-    return (
-        <img src={ImgLogo} alt="Logo Kasa" />
-    )
-}
-
-export default Logo;
