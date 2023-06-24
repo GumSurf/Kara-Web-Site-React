2b06edb7bf0a0df2efc146aa8ee8e46fc72a4b65
GumSurf <gabriel.christe@gmail.com>
2023-06-24 02:39:05 +0200

    Add : clic logo envoie sur la page d'accueil

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
-import ImgLogo from '../assets/LOGO.png'
+import ImgLogo from '../assets/LOGO.png';
 import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div className="kasa-header">
-            <img className="header-logo" src={ImgLogo} alt="Logo Kasa" />
-            <ul className='header-links'>
+            <Link to="/">
+                <img className="header-logo" src={ImgLogo} alt="Logo Kasa" />
+            </Link>
+            <ul className="header-links">
                 <li>
-                    <Link className='header-link-accueil' to="/">Accueil</Link>
+                    <Link className="header-link-accueil" to="/">
+                        Accueil
+                    </Link>
                 </li>
                 <li>
-                    <Link className='header-link-a-propos' to="/about">A propos</Link>
+                    <Link className="header-link-a-propos" to="/about">
+                        A propos
+                    </Link>
                 </li>
             </ul>
         </div>
