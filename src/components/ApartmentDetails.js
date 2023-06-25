632d2fa7125ca237d4b40b138c49db2c488dcff9
GumSurf <gabriel.christe@gmail.com>
2023-06-26 00:40:19 +0200

    Add : arrow left et right pour switch d'appartement

--- src/components/ApartmentDetails.js
+++ src/components/ApartmentDetails.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import data from '../datas/LogementList.json';
 import RatingStars from './RatingStars';
+import ArrowLeft from '../assets/ArrowLeft.png'
+import ArrowRight from '../assets/ArrowRight.png'
 
 function ApartmentDetails() {
     const params = useParams();
@@ -16,21 +18,50 @@ function ApartmentDetails() {
     const firstName = nameParts[0];
     const lastName = nameParts.slice(1).join(' ');
 
+    //Next Apartment
+    const currentIndex = data.findIndex((item) => item.id === id);
+
+    let nextApartment;
+    if (currentIndex !== -1 && currentIndex < data.length - 1) {
+        nextApartment = data[currentIndex + 1];
+    } else {
+        nextApartment = data[0];
+    }
+
+    const nextApartmentLink = `/details_appartement/${nextApartment.id}`;
+
+    //Previous Apartment
+    let previousApartment;
+    if (currentIndex !== -1 && currentIndex > 0) {
+        previousApartment = data[currentIndex - 1];
+    } else {
+        previousApartment = data[data.length - 1];
+    }
+
+    const previousApartmentLink = `/details_appartement/${previousApartment.id}`;
+
     return (
         <div className='div-apartment'>
             <div className='div-img-apartment'>
                 <img className='img-apartment' src={apartment.cover} alt={apartment.title} />
+                <div className='div-links'>
+                    <Link className='link' to={previousApartmentLink}>
+                        <img src={ArrowLeft} alt="Lien précédent" className="link-image" />
+                    </Link>
+                    <Link className='link' to={nextApartmentLink}>
+                        <img src={ArrowRight} alt="Lien suivant" className="link-image" />
+                    </Link>
+                </div>
             </div>
-
             <div className='title-host-info'>
                 <div className='div-title-location'>
-                    <h2 className='titre-apartment'>{apartment.title}</h2>
+                    <h1 className='titre-apartment'>{apartment.title}</h1>
                     <p className='location-apartment'>{apartment.location}</p>
                 </div>
                 <div className='host-info'>
                     <div className='div-names'>
-                        <h3 className='host-names'>{firstName}</h3>
-                        <h3 className='host-names'>{lastName}</h3>
+                        <p className='host-names'>{firstName}</p>
+                        <p className='host-names'>{lastName}</p>
                     </div>
                     <div className='div-img-host'>
                         <img className='host-img' src={apartment.host.picture} alt={apartment.host.name} />
@@ -48,22 +79,23 @@ function ApartmentDetails() {
                 </div>
                 <RatingStars rating={apartment.rating} />
             </div>
-
             <div className='div-details-apartment'>
-                <details className='details-apartment details-description'>
-                    <summary className='summary-animation'>Description</summary>
-                    <p className='description-apartment'>{apartment.description}</p>
-                </details>
+            <details>
+                <summary className='summary-animation'>Description</summary>
+                <p className='description-apartment'>{apartment.description}</p>
+            </details>
 
-                <details className='details-apartment details-equipment'>
-                    <summary className='summary-animation'>Equipment</summary>
-                    <ul className='ul-equipment'>
-                        {apartment.equipments.map((equipment, index) => (
-                            <li className='equipment-apartment' key={index}>{equipment}</li>
-                        ))}
-                    </ul>
-                </details>
-            </div>
+            <details>
+                <summary className='summary-animation'>Equipment</summary>
+                <ul className='ul-equipment'>
+                    {apartment.equipments.map((equipment, index) => (
+                        <li className='equipment-apartment' key={index}>
+                            {equipment}
+                        </li>
+                    ))}
+                </ul>
+            </details>
+        </div>
         </div>
     );
 }
