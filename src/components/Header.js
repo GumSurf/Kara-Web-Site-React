5df4193c0b6105e8cf74621d82017913c0d57f5b
GumSurf <gabriel.christe@gmail.com>
2023-06-26 17:01:53 +0200

    Add : className for scss

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <div className="kasa-header">
-            <Link to="/">
+            <Link className='link-logo' to="/">
                 <img className="header-logo" src={ImgLogo} alt="Logo Kasa" />
             </Link>
             <ul className="header-links">
-                <li>
+                <li className='li-links'>
                     <Link className="header-link-accueil" to="/">
                         Accueil
                     </Link>
                 </li>
-                <li>
+                <li className='li-links'>
                     <Link className="header-link-a-propos" to="/about">
                         A propos
                     </Link>
