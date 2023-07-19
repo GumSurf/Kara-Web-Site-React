e59370c3def048b104713bfd52b6fba116f49798
GumSurf <gabriel.christe@gmail.com>
2023-07-19 17:58:46 +0200

    Add: error 404

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ function App() {
         <Route path="/Kasa-Web-Site-React/about" element={<About />} />
         <Route path="/Kasa-Web-Site-React/details_appartement/:id" element={<ApartmentDetails />} />
         <Route path="/Kasa-Web-Site-React/*" element={<NotFound />}/>
-        <Route path="*" element={<NotFound />}/>
+        <Route path="/*" element={<NotFound />}/>
       </Routes>
 
       <Footer />
